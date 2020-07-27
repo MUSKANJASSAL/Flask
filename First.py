390ec7c04b152741735b0490183e736e3b36b440
muskan jassal <muskan124.jassal@gmail.com>
2020-07-27 23:46:01 +0530

    first commit

--- First.py
+++ First.py
@@ -1,12 +1,34 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 # Instance of flask web application
 app = Flask(__name__)
 
+# Dynamic
+# @app.route("/<name>")
 @app.route("/")
 # Defining the pages on the website
+# def home(name):
 def home():
-	return "Hello World!"
+	# return "Hello World!"
+	# return render_template("index.html", content=name)
+	# return render_template("index.html", content=["tim", "joe", "gorge"])
+	return render_template("index.html")
+
+@app.route("/test")
+def test():
+	return render_template("new.html", content="Testing")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+	if request.method == "POST":
+		user = request.form["name"]
+		return redirect(url_for("user", usr=user))
+	else:
+		return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+	return "<h1>{usr}</h1>"
 
 # @app.route("/<name>")
 # def user(name):
@@ -20,4 +42,5 @@ def home():
 
 # Run the app
 if __name__== "__main__":
-	app.run()
+	# Automatically detecting changes and updating the website
+	app.run(debug=True)
